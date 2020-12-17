cc1e84a5e15cc2b1ba3371cdd6d5a7df5c52d0d3
xtbo97 <xtbo9719@gmail.com>
2020-12-17 12:17:00 +0800

    用户注册-短信验证码发送功能出初步实现-pipeline操作redis数据库

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -52,8 +52,6 @@ class SMSCodeView(View):
 
         # 2.对比图片验证码
         # 获取redis中的图形验证码
-        redis_conn = get_redis_connection('verify_code')
-
         image_code_redis = redis_conn.get('img_%s' % uuid)
 
         if image_code_redis is None:
@@ -74,11 +72,18 @@ class SMSCodeView(View):
         sms_code = '%06d' % random.randint(0, 999999)
         logger.info('短信验证码为: %s' % sms_code)
 
+        # 创建redis pipeline管道
+        pl = redis_conn.pipeline()
+
+        # 将redis请求操作添加到队列
         # 保存短信验证码
-        redis_conn.set('sms_%s' % mobile, sms_code, 300)
+        pl.set('sms_%s' % mobile, sms_code, 300)
 
         # 设置短信发送的标记,有效期为:60s
-        redis_conn.set('send_flag_%s' % mobile, 1, 60)
+        pl.set('send_flag_%s' % mobile, 1, 60)
+
+        # 执行redis pipeline请求
+        pl.execute()
 
         # 发送短信验证码
         CCP().send_template_sms(mobile, [sms_code, 5], 1)
