fae97775daf6425ca35d3bc78e08930646164b71
xtbo97 <xtbo9719@gmail.com>
2020-12-20 19:59:35 +0800

    用户中心-收货地址管理-省市县三级联动-指定地区下级地区数据的获取

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -20,3 +20,22 @@ class ProvinceAreasView(View):
         return JsonResponse({'code': 0,
                              'message': 'OK',
                              'provinces': provinces, })
+
+
+# GET /areas/(?P<pk>\d+)/
+class SubAreasView(View):
+    def get(self, request, pk):
+        """ 获取指定地区的下级地区信息 """
+        # 1.根据pk获取指定地区的下级地区信息
+        try:
+            subs_li = Areas.objects.filter(parent_id=pk).values('id', 'name')
+            # 将QuerySet中的数据转换为列表
+            subs_li = list(subs_li)
+        except Exception as e:
+            return JsonResponse({'code': 400,
+                                 'message': '子级地区信息获取错误!'})
+
+        # 2.组织数据并进行返回
+        return JsonResponse({'code': 0,
+                             'message': 'OK',
+                             'subs': subs_li})
