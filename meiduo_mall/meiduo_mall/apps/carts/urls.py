d2be5655ac59cafaeb836354e3e41da55f3555fc
xtbo97 <xtbo9719@gmail.com>
2020-12-27 10:57:03 +0800

    购物车模块-购物车增删改查功能实训

---
+++ meiduo_mall/meiduo_mall/apps/carts/urls.py
@@ -0,0 +1,12 @@
+"""
+xtbo97
+"""
+from django.urls import re_path
+
+from carts import views
+
+urlpatterns = [
+    re_path(r'^carts/$', views.CartView.as_view()),
+    re_path(r'^carts/selection/$', views.CartSelectView.as_view()),
+
+]
