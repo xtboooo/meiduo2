bbcc4e8727c3670f1c67ab7e08b61d4372d8ae6a
xtbo97 <xtbo9719@gmail.com>
2020-12-27 11:04:53 +0800

    作业-商品详情页热销TOP2商品展示

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -8,5 +8,6 @@ from goods import views
 urlpatterns = [
     re_path(r'^list/(?P<category_id>\d+)/skus/$', views.SKUListView.as_view()),
     re_path(r'^search/$', views.SKUSearchView.as_view()),
+    re_path(r'^hot/(?P<category_id>\d+)/$', views.GetHotSKUView.as_view()),
 
 ]
