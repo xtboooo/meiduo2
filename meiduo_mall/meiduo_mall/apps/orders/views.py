369aa110ffcdcfd9c5a5c3537a0315cef6517d7f
xtbo97 <xtbo9719@gmail.com>
2020-12-28 21:04:59 +0800

    订单模块-订单数据保存基本功能-事务的使用

--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -127,54 +127,59 @@ class OrderCommitView(LoginRequiredMixin, View):
         # 运费(此处固定)
         freight = Decimal(10.00)
 
-        # ③ 向 tb_order_info 数据表中添加一行记录
-        order = OrderInfo.objects.create(order_id=order_id,
-                                         user=user,
-                                         address=address,
-                                         total_count=total_count,
-                                         total_amount=total_amount,
-                                         freight=freight,
-                                         pay_method=pay_method,
-                                         status=status)
+        with transaction.atomic():
+            # 设置数据库操作时，事务中的保存点
+            sid = transaction.savepoint()
+            # ③ 向 tb_order_info 数据表中添加一行记录
+            order = OrderInfo.objects.create(order_id=order_id,
+                                             user=user,
+                                             address=address,
+                                             total_count=total_count,
+                                             total_amount=total_amount,
+                                             freight=freight,
+                                             pay_method=pay_method,
+                                             status=status)
 
-        # ④ 遍历用户要购买的商品记录，循环向 tb_order_goods 表中添加记录
-        # 从 redis 中获取用户要购买的商品信息
-        cart_helper = CartHelper(request)
-        cart_dict = cart_helper.get_redis_selected_cart()
-        sku_ids = cart_dict.keys()
+            # ④ 遍历用户要购买的商品记录，循环向 tb_order_goods 表中添加记录
+            # 从 redis 中获取用户要购买的商品信息
+            cart_helper = CartHelper(request)
+            cart_dict = cart_helper.get_redis_selected_cart()
+            sku_ids = cart_dict.keys()
 
-        for sku_id in sku_ids:
-            sku = SKU.objects.get(id=sku_id)
-            count = cart_dict[sku_id]
+            for sku_id in sku_ids:
+                sku = SKU.objects.get(id=sku_id)
+                count = cart_dict[sku_id]
 
-            # 判断库存是否充足
-            if count > sku.stock:
-                return JsonResponse({'code': 400,
-                                     'message': '商品库存不足'})
+                # 判断库存是否充足
+                if count > sku.stock:
+                    # 数据库操作时，撤销事务中指定保存点之后的操作
+                    transaction.savepoint_rollback(sid)
+                    return JsonResponse({'code': 400,
+                                         'message': '商品库存不足'})
 
-            # 减少SKU商品库存、增加销量
-            sku.stock -= count
-            sku.sales += count
-            sku.save()
+                # 减少SKU商品库存、增加销量
+                sku.stock -= count
+                sku.sales += count
+                sku.save()
 
-            # 增加对应SPU商品的销量
-            sku.spu.sales += count
-            sku.spu.save()
+                # 增加对应SPU商品的销量
+                sku.spu.sales += count
+                sku.spu.save()
 
-            # 保存订单商品信息
-            OrderGoods.objects.create(order=order,
-                                     sku=sku,
-                                     count=count,
-                                     price=sku.price, )
+                # 保存订单商品信息
+                OrderGoods.objects.create(order=order,
+                                          sku=sku,
+                                          count=count,
+                                          price=sku.price, )
 
-            # 累加计算订单商品的总数量和总价格
-            total_count += count
-            total_amount += count * sku.price
+                # 累加计算订单商品的总数量和总价格
+                total_count += count
+                total_amount += count * sku.price
 
-        total_amount += freight
-        order.total_count = total_count
-        order.total_amount = total_amount
-        order.save()
+            total_amount += freight
+            order.total_count = total_count
+            order.total_amount = total_amount
+            order.save()
 
         # ⑤ 清除用户购物车中已购买的记录
         cart_helper.clear_redis_selected_cart()
@@ -183,4 +188,3 @@ class OrderCommitView(LoginRequiredMixin, View):
         return JsonResponse({'code': 0,
                              'message': '下单成功',
                              'order_id': order_id})
-
