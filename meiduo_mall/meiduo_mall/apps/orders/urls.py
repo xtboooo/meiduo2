ddfcfff9cc532f0ece1b5a208997f800e8c4d663
xtbo97 <xtbo9719@gmail.com>
2020-12-28 20:58:21 +0800

    订单模块-订单数据保存基本功能API实现

--- meiduo_mall/meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -4,5 +4,6 @@ from orders import views
 
 urlpatterns = [
     re_path(r'^orders/settlement/$', views.OrderSettlementView.as_view()),
+    re_path(r'^orders/commit/$', views.OrderCommitView.as_view()),
 
 ]
