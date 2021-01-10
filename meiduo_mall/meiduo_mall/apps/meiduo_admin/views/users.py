c5336c43c631447693517ed691e1af422bb05abe
xtbo97 <xtbo9719@gmail.com>
2021-01-10 12:25:29 +0800

    美多后台项目-管理员登陆-管理员登录API代码优化(子类试图)

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/users.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -6,17 +7,17 @@ from meiduo_admin.serializers.users import AdminAuthSerializer
 
 
 # POST /meiduo_admin/authorizations/
-class AdminAuthorizeView(APIView):
-    def post(self, request):
-        """管理员登录"""
-        # ① 获取参数并进行校验
-        serializer = AdminAuthSerializer(data=request.data)
-        serializer.is_valid()
-
-        # ② 服务器生成 jwt token 数据
-        # 调用序列化器类中的 create，将生成 JWT token 的代码抽取到了序列化器类的 create 方法中
-        serializer.save()
-
-        # ③ 返回响应
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
+class AdminAuthorizeView(CreateAPIView):
+    serializer_class = AdminAuthSerializer
+    # def post(self, request):
+    #     """管理员登录"""
+    #     # ① 获取参数并进行校验
+    #     serializer = AdminAuthSerializer(data=request.data)
+    #     serializer.is_valid()
+    #
+    #     # ② 服务器生成 jwt token 数据
+    #     # 调用序列化器类中的 create，将生成 JWT token 的代码抽取到了序列化器类的 create 方法中
+    #     serializer.save()
+    #
+    #     # ③ 返回响应
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
