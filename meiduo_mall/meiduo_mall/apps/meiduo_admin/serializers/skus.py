90acd04fca6261b71a9bd1fb58d7aa887e604db4
xtbo97 <xtbo9719@gmail.com>
2021-01-13 12:02:12 +0800

    美多后台项目-商品管理-SKU商品图片管理-SKU商品图片数据新增+修改+删除

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/skus.py
@@ -11,6 +11,28 @@ class SKUImageSerializer(serializers.ModelSerializer):
         model = SKUImage
         exclude = ('create_time', 'update_time')
 
+    def validate_sku_id(self, value):
+        """针对 sku_id 进行补充验证"""
+        # SKU商品是否存在
+        try:
+            sku = SKU.objects.get(id=value)
+        except SKU.DoesNotExist:
+            raise serializers.ValidationError('SKU商品不存在')
+        return value
+
+    def create(self, validated_data):
+        """上传 SKU 商品图片保存"""
+        # 调用 ModelSerializer 中的 create 方法，进行上传文件保存和表记录添加
+        sku_image = super().create(validated_data)
+
+        # 判断是否需要设置 SKU 商品的默认图片
+        sku = sku_image.sku
+
+        if not sku.default_image:
+            sku.default_image = sku_image
+            sku.save()
+        return sku_image
+
 
 class SKUSimpleSerializer(serializers.ModelSerializer):
     class Meta:
