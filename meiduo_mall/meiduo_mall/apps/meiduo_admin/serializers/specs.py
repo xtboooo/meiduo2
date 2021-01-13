2990de61383697df81bd333cf7f9ee22668a81ca
xtbo97 <xtbo9719@gmail.com>
2021-01-13 22:30:06 +0800

    美多后台实训-商品管理-规格管理-规格数据新增+修改+删除

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/specs.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/specs.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 
-from goods.models import SPUSpecification
+from goods.models import SPUSpecification, SPU
 
 
 class SpecSerializer(serializers.ModelSerializer):
     """商品规格序列化器类"""
+    spu = serializers.StringRelatedField(label='spu商品名称', read_only=True)
+    spu_id = serializers.IntegerField(label='spu商品id')
 
     class Meta:
         model = SPUSpecification
         exclude = ('create_time', 'update_time')
+
+    def validate_spu_id(self, value):
+        try:
+            spu = SPU.objects.get(id=value)
+        except SPU.DoesNotExist:
+            raise serializers.ValidationError('spu_id参数有误')
+        return value
