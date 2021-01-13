30947846012b5712481d1c0535cbfe597d672bea
xtbo97 <xtbo9719@gmail.com>
2021-01-13 20:06:24 +0800

    美多后台实训-spu商品管理-spu商品新增+修改+删除

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/spus.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/spus.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from goods.models import SPU, Brand
+from goods.models import SPU, Brand, GoodsCategory
 
 
 class SPUSerializer(serializers.ModelSerializer):
@@ -10,9 +10,45 @@ class SPUSerializer(serializers.ModelSerializer):
     category2_id = serializers.IntegerField(label='二级分类id')
     category3_id = serializers.IntegerField(label='三级分类id')
 
+    extra_kwargs = {
+        'sales': {
+            'read_only': True
+        },
+        'comments': {
+            'read_only': True
+        }
+    }
+
     class Meta:
         model = SPU
         exclude = ('create_time', 'update_time', 'category1', 'category2', 'category3')
+
+    def validate(self, attrs):
+        brand_id = attrs.get('brand_id')
+        category1_id = attrs.get('category1_id')
+        category2_id = attrs.get('category2_id')
+        category3_id = attrs.get('category3_id')
+        try:
+            brand = Brand.objects.get(id=brand_id)
+        except Brand.DoesNotExist:
+            raise serializers.ValidationError('brand_id参数有误')
+
+        try:
+            category1 = GoodsCategory.objects.get(id=category1_id, parent_id=None)
+        except GoodsCategory.DoesNotExist:
+            raise serializers.ValidationError('category1_id参数有误')
+
+        try:
+            category2 = GoodsCategory.objects.get(id=category2_id, parent_id__in=list(range(1, 38)))
+        except GoodsCategory.DoesNotExist:
+            raise serializers.ValidationError('category2_id参数有误')
+
+        try:
+            category3 = GoodsCategory.objects.get(id=category3_id, parent_id__in=list(range(38, 115)))
+        except GoodsCategory.DoesNotExist:
+            raise serializers.ValidationError('category3_id参数有误')
+
+        return attrs
 
 
 class BrandSerializer(serializers.ModelSerializer):
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -35,6 +35,9 @@ urlpatterns = [
     # 商品管理-spu商品管理-获取品牌数据
     re_path('^goods/brands/simple/$', spus.BrandView.as_view()),
 
+    # 商品管理-spu商品管理-上传spu图片
+    # TODO: 上传spu图片
+
     # 权限管理-权限类型数据获取
     re_path(r'^permission/content_types/$', permissions.PermissionViewSet.as_view({
         'get': 'content_types'
